be51853179cdb353d41d4c3239fea37e2aa92428
Zach <zacharyg88@gmail.com>
2024-06-07 09:24:33 -0400

    Initial Commit

---
+++ CutCalc/CutCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CutCalcApp.swift
+//  CutCalc
+//
+//  Created by Zach Eidenberger on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CutCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
